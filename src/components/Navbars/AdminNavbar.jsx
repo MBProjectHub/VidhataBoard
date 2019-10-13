2b58dd709634fa51af172a024167e08c1c35d743
bharg <bhargavauden1998@gmail.com>
2019-10-13 17:00:24 +0530

    NavBar and Footer

--- src/components/Navbars/AdminNavbar.jsx
+++ src/components/Navbars/AdminNavbar.jsx
@@ -34,21 +34,17 @@ import {
   Container,
   Media
 } from "reactstrap";
+import { NONAME } from "dns";
 
 class AdminNavbar extends React.Component {
-  render() {
-    return (
-      <>
-        <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
-          <Container fluid>
-            <Link
-              className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block"
-              to="/"
-            >
-              {this.props.brandText}
-            </Link>
-            <Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
-              <FormGroup className="mb-0">
+
+  loadSearch()
+  {
+    let searchVisibility = "hidden"
+    if(this.props.brandText == "Bookings")
+      searchVisibility = "visible"
+    return <Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto" style={{visibility:searchVisibility}}>
+              <FormGroup >
                 <InputGroup className="input-group-alternative">
                   <InputGroupAddon addonType="prepend">
                     <InputGroupText>
@@ -59,50 +55,24 @@ class AdminNavbar extends React.Component {
                 </InputGroup>
               </FormGroup>
             </Form>
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main" style={{marginTop:'5px'}}>
+          <Container fluid>
+            <Link
+              className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block"
+              to="/"
+            >
+              {this.props.brandText}
+            </Link>
+            {this.loadSearch()}
             <Nav className="align-items-center d-none d-md-flex" navbar>
-              <UncontrolledDropdown nav>
-                <DropdownToggle className="pr-0" nav>
-                  <Media className="align-items-center">
-                    <span className="avatar avatar-sm rounded-circle">
-                      <img
-                        alt="..."
-                        src={require("assets/img/theme/team-4-800x800.jpg")}
-                      />
-                    </span>
-                    <Media className="ml-2 d-none d-lg-block">
-                      <span className="mb-0 text-sm font-weight-bold">
-                        Jessica Jones
-                      </span>
-                    </Media>
-                  </Media>
-                </DropdownToggle>
-                <DropdownMenu className="dropdown-menu-arrow" right>
-                  <DropdownItem className="noti-title" header tag="div">
-                    <h6 className="text-overflow m-0">Welcome!</h6>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-single-02" />
-                    <span>My profile</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-settings-gear-65" />
-                    <span>Settings</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-calendar-grid-58" />
-                    <span>Activity</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-support-16" />
-                    <span>Support</span>
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem href="#pablo" onClick={e => e.preventDefault()}>
-                    <i className="ni ni-user-run" />
-                    <span>Logout</span>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
+                <span className="mb-0 font-weight-bold" style={{color:'#fff', fontSize:'17px'}}>
+                  Jessica Jones
+                </span>
             </Nav>
           </Container>
         </Navbar>
